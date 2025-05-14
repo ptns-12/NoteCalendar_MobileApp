6c9eb61edd04a60a02bd2a8713411ee08fc4c4c1
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-05-15 01:27:19 +0700

    run app

--- NoteCalendar/app/src/main/java/ptns/ntu/notecalendar/MainActivity.java
+++ NoteCalendar/app/src/main/java/ptns/ntu/notecalendar/MainActivity.java
@@ -1,12 +1,19 @@
 package ptns.ntu.notecalendar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +22,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                if (item.getItemId() == R.id.nav_calendar) {
+                    selectedFragment = new CalendarFragment();
+                } else if (item.getItemId() == R.id.nav_notes) {
+                    selectedFragment = new NotesFragment();
+                } else if (item.getItemId() == R.id.nav_done) {
+                    selectedFragment = new DoneFragment();
+                }
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frame_layout, selectedFragment)
+                            .commit();
+                }
+                return true;
+            }
         });
+
     }
 }
