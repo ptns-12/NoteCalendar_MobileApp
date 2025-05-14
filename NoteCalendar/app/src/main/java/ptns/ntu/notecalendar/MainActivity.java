2fd75aa43409e54222c4a8047c6c6fe215e22704
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-05-15 01:43:32 +0700

    update calendarfragment

--- NoteCalendar/app/src/main/java/ptns/ntu/notecalendar/MainActivity.java
+++ NoteCalendar/app/src/main/java/ptns/ntu/notecalendar/MainActivity.java
@@ -23,12 +23,26 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Hiển thị CalendarFragment làm màn hình mặc định khi khởi chạy
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame_layout, new CalendarFragment())
+                    .commit();
+            //Đặt trạng thái "Selected" cho Calendar
+            bottomNavigationView.setSelectedItemId(R.id.nav_calendar);
+        }
+
+        // Lắng nghe sự kiện khi người dùng chọn item
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @SuppressLint("NonConstantResourceId")
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment selectedFragment = null;
+
+                // Xử lý sự kiện chọn tab
                 if (item.getItemId() == R.id.nav_calendar) {
                     selectedFragment = new CalendarFragment();
                 } else if (item.getItemId() == R.id.nav_notes) {
@@ -37,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                     selectedFragment = new DoneFragment();
                 }
 
+                // Thay thế fragment trong FrameLayout
                 if (selectedFragment != null) {
                     getSupportFragmentManager().beginTransaction()
                             .replace(R.id.frame_layout, selectedFragment)
